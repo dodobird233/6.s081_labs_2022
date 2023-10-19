e915e1f58bbca2e6e72e0ba7b4bd2074d7f14f50
dodobird233 <mariohong128@outlook.com>
2023-10-19 15:07:31 +0800

    finish fs

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -323,6 +323,7 @@ sys_open(void)
       return -1;
     }
   } else {
+    int depth=0;
     if((ip = namei(path)) == 0){
       end_op();
       return -1;
@@ -332,6 +333,24 @@ sys_open(void)
       iunlockput(ip);
       end_op();
       return -1;
+    }
+    while(ip->type==T_SYMLINK && !(omode & O_NOFOLLOW)){
+      if(readi(ip,0,(uint64)path,0,MAXPATH)<0){
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+      iunlockput(ip);
+      if((ip = namei(path)) == 0){
+        end_op();
+        return -1;
+      }
+      ilock(ip);
+      if(++depth>10){
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
     }
   }
 
@@ -501,5 +520,34 @@ sys_pipe(void)
     fileclose(wf);
     return -1;
   }
+  return 0;
+}
+
+
+uint64
+sys_symlink(void){
+  char target[MAXPATH], path[MAXPATH];
+  struct inode *ip;
+
+  if(argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
+    return -1;
+
+  begin_op();
+  if((ip = namei(path)) != 0){//already exist
+    end_op();
+    return -1;
+  }
+  ip=create(path,T_SYMLINK,0,0);//allocate a new inode for symlink
+  if(ip==0){
+    end_op();
+    return -1;
+  }
+  if(writei(ip,0,(uint64)target,0,MAXPATH)<0){ //write target to the new inode[0,MAXPATH]
+    iunlockput(ip);
+    end_op();
+    return -1;
+  }
+  iunlockput(ip);
+  end_op();
   return 0;
 }
